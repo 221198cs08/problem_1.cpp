d2c8465bcc3607d58917ebe9c48d7452d6c36fa1
Azizbek Boboqulov <113372382+221198cs20@users.noreply.github.com>
2023-02-24 12:46:40 +0500

    Update problem_3.cpp

--- problem_3.cpp
+++ problem_3.cpp
@@ -3,12 +3,16 @@ using namespace std;
 
 int main(){
     float english=4.0;
+    cin>>english;
     float english_credit=3;
     float programming=3.7;
+    cin>>programming;
     float programming_credit=2;
     float math=3.3;
+    cin>>math;
     float math_credit=2;
     float GPA;
+    
     GPA=english*english_credit+programming*programming_credit+math*math_credit/english_credit+programming_credit+math_credit;
     cout<<GPA<<endl;
     return 0;
